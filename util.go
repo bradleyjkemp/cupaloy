8777feb7bc7d835823a5ef158edb3c4c618da7e9
bradleyjkemp <scytheon3@gmail.com>
2017-08-09 21:38:44 +0000

    Allow for configuration

--- util.go
+++ util.go
@@ -2,6 +2,8 @@ package cupaloy
 
 import (
 	"errors"
+	"fmt"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/pmezard/go-difflib/difflib"
 	"io/ioutil"
 	"os"
@@ -10,10 +12,13 @@ import (
 	"strings"
 )
 
-const (
-	subDirName        = ".snapshots"
-	snapshotExtension = ""
-)
+var spewConfig = spew.ConfigState{
+	Indent:                  "  ",
+	SortKeys:                true, // maps should be spewed in a deterministic order
+	DisablePointerAddresses: true, // don't spew the addresses of pointers
+	DisableCapacities:       true, // don't spew capacities of collections
+	SpewKeys:                true, // if unable to sort map keys then spew keys to strings and sort those
+}
 
 func getNameOfCaller() string {
 	pc, _, _, _ := runtime.Caller(2) // first caller is the caller of this function, we want the caller of our caller
@@ -28,20 +33,20 @@ func shouldUpdate() bool {
 	return varSet
 }
 
-func snapshotFilePath(testName string) string {
-	return filepath.Join(subDirName, testName+snapshotExtension)
+func (c *Config) snapshotFilePath(testName string) string {
+	return filepath.Join(c.subDirName, testName+c.snapshotExtension)
 }
 
 func takeSnapshot(i ...interface{}) string {
-	return spewConfig.Sdump(i)
+	return spewConfig.Sdump(i...)
 }
 
-func readSnapshot(testName string) (string, error) {
-	snapshotFile := snapshotFilePath(testName)
+func (c *Config) readSnapshot(snapshotName string) (string, error) {
+	snapshotFile := c.snapshotFilePath(snapshotName)
 	buf, err := ioutil.ReadFile(snapshotFile)
 
 	if os.IsNotExist(err) {
-		return "", errors.New("no snapshot exists for test " + testName)
+		return "", fmt.Errorf("no snapshot exists for test %s", snapshotName)
 	}
 
 	if err != nil {
@@ -51,14 +56,14 @@ func readSnapshot(testName string) (string, error) {
 	return string(buf), nil
 }
 
-func writeSnapshot(testName string, snapshot string) error {
-	// check that ./snapshots/ directory exists before writing snapshot
-	err := os.MkdirAll(subDirName, os.ModePerm)
+func (c *Config) writeSnapshot(snapshotName string, snapshot string) error {
+	// check that subdirectory exists before writing snapshot
+	err := os.MkdirAll(c.subDirName, os.ModePerm)
 	if err != nil {
 		return errors.New("could not create snapshots directory")
 	}
 
-	snapshotFile := snapshotFilePath(testName)
+	snapshotFile := c.snapshotFilePath(snapshotName)
 	return ioutil.WriteFile(snapshotFile, []byte(snapshot), os.FileMode(0644))
 }
 
