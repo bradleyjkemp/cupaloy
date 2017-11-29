5847e1e3b68235a8f76b8028956f9de0895ae0d2
Bradley Kemp <bradleyjkemp@users.noreply.github.com>
2017-11-29 16:24:45 +0000

    Add SnapshotT function and make tests cross platform (#15)
    * Add SnapshotT function and make tests cross platform
    
    * Add missing snapshots
    
    * Add godoc

--- cupaloy.go
+++ cupaloy.go
@@ -3,6 +3,8 @@ package cupaloy
 import (
 	"fmt"
 	"os"
+	"strings"
+	"testing"
 )
 
 // Snapshotter is the API for taking snapshots of values in your tests.
@@ -15,6 +17,10 @@ type Snapshotter interface {
 	// SnapshotMulti is identical to Snapshot but can be called multiple times from the same function.
 	// This is done by providing a unique snapshotId for each invocation.
 	SnapshotMulti(snapshotID string, i ...interface{}) error
+
+	// SnapshotT is identical to Snapshot but gets the snapshot name using
+	// t.Name() and calls t.Fail() directly if the snapshots do not match.
+	SnapshotT(t *testing.T, i ...interface{})
 }
 
 // New constructs a new, configured instance of cupaloy using the given Configurators.
@@ -39,6 +45,16 @@ func SnapshotMulti(snapshotID string, i ...interface{}) error {
 	return defaultConfig().snapshot(snapshotName, i...)
 }
 
+// SnapshotT calls Snapshotter.SnapshotT with the default config.
+func SnapshotT(t *testing.T, i ...interface{}) {
+	t.Helper()
+	snapshotName := strings.Replace(t.Name(), "/", "-", -1)
+	err := defaultConfig().snapshot(snapshotName, i...)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
 func (c *config) Snapshot(i ...interface{}) error {
 	return c.snapshot(getNameOfCaller(), i...)
 }
@@ -46,6 +62,15 @@ func (c *config) Snapshot(i ...interface{}) error {
 func (c *config) SnapshotMulti(snapshotID string, i ...interface{}) error {
 	snapshotName := fmt.Sprintf("%s-%s", getNameOfCaller(), snapshotID)
 	return c.snapshot(snapshotName, i...)
+}
+
+func (c *config) SnapshotT(t *testing.T, i ...interface{}) {
+	t.Helper()
+	snapshotName := strings.Replace(t.Name(), "/", "-", -1)
+	err := c.snapshot(snapshotName, i...)
+	if err != nil {
+		t.Error(err)
+	}
 }
 
 func (c *config) snapshot(snapshotName string, i ...interface{}) error {
